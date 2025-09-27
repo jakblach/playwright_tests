8242f561fb4c4405abd94aed008a10ef74c7c969
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-09-27 22:36:41 +0200

    Payment section

--- resources/validationMessages.ts
+++
@@ -1,3 +0,0 @@
-Real World App requires a Bank Account to perform transactions.
-
-Click Next to begin setup of your Bank Account.
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,9 +1,10 @@
-import { test, expect } from '@playwright/test';
+// import { test, expect } from '@playwright/test';
 import { LoginPage } from '../resources/loginPage';
 import { log } from 'console';
 import { WelcomePage } from '../resources/welcomPage';
 import { BankAccountCreatePage } from '../resources/createAccount';
 import { TransactionPage } from '../resources/tansactionPage';
+import { test, expect } from '../fixtures/auth-fixture';
 
 
 
@@ -29,35 +30,35 @@ import { TransactionPage } from '../resources/tansactionPage';
 //     await loginPage.createNewAccount();
 //     await loginPage.fillFirstName('JAKUBON123');
 //     await loginPage.fillLastName('SZEFU');
-//     await loginPage.fillUsername('test4444');
+//     await loginPage.fillUsername('test001');
 //     await loginPage.fillPassword('s3cret1');
 //     await loginPage.fillConfirmPassword('s3cret1');
 //     await loginPage.SignIn();
     
 // });
 
-// test('log in as new user and create bank account', async ({page}) => {
-//   const loginPage = new LoginPage(page);
-//   const testWelcome = new WelcomePage(page);
-//   const testBank = new BankAccountCreatePage(page);
-//   await loginPage.LoginPage();
-//   await loginPage.logOn('test4444', 's3cret1');
-//   await testWelcome.waitForOpen();
-//   await testWelcome.clickNextButtonOnWelcomePage();
-//   await testBank.setBankName('JAKUBON SIGMOZA');
-//   await testBank.setRoutingNumber('123456789');
-//   await testBank.setAccountNumber('123456789');
-//   await testBank.clickButtonSave();
-//   await testWelcome.clickDoneButtonOnWelcomePage();
-
-// });
-
-test('log in and check if transaction list is visible', async ({page}) => {
-  const loginPage = new LoginPage(page);
-  const transaction = new TransactionPage(page);
-
-  await loginPage.LoginPage();
-  await loginPage.logOn('test4444', 's3cret1');
-  await transaction.expectTransactionListVisible();
+test('log in as new user and create bank account', async ({loggedInPage}) => {
+  const loginPage = new LoginPage(loggedInPage);
+  const testWelcome = new WelcomePage(loggedInPage);
+  const testBank = new BankAccountCreatePage(loggedInPage);
+  // await loginPage.LoginPage();
+  // await loginPage.logOn('test4444', 's3cret1');
+  await testWelcome.waitForOpen();
+  await testWelcome.clickNextButtonOnWelcomePage();
+  await testBank.setBankName('JAKUBON SIGMOZA');
+  await testBank.setRoutingNumber('123456789');
+  await testBank.setAccountNumber('123456789');
+  await testBank.clickButtonSave();
+  await testWelcome.clickDoneButtonOnWelcomePage();
 
 });
+
+// test('log in and check if transaction list is visible', async ({page}) => {
+//   const loginPage = new LoginPage(page);
+//   const transaction = new TransactionPage(page);
+
+//   await loginPage.LoginPage();
+//   await loginPage.logOn('test4444', 's3cret1');
+//   await transaction.expectTransactionListVisible();
+
+// });
